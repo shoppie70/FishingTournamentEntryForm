cff729ccb59543164bbefd6b1e9c712b6732786d
Sho Tsukamoto <sho.tsuka.0709@gmail.com>
2022-07-18 14:58:13 +0900

    Fixed Select box Text Align.

--- Modules/Front/Resources/views/index.blade.php
+++ Modules/Front/Resources/views/index.blade.php
@@ -164,7 +164,7 @@
                             大会終了後の親睦会への参加
                         </label>
                         <select name="fellowship" id="fellowship"
-                                class="text-right rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full">
+                                class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full">
                             <option value="1">
                                 参加する
                             </option>
