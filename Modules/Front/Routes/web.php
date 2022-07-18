426cb9b6652caad881a2e3640c5ded14b452acb3
Sho Tsukamoto <sho.tsuka.0709@gmail.com>
2022-07-18 12:13:19 +0900

    Fixed Route Name.

--- Modules/Front/Routes/web.php
+++ Modules/Front/Routes/web.php
@@ -14,6 +14,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Front\Http\Controllers\FrontController;
 
-Route::get('/', [FrontController::class, 'index'])->name('user.index');
+Route::get('/', [FrontController::class, 'index'])->name('index');
 Route::get('/entry', [FrontController::class, 'entry'])->name('entry');
 Route::post('/entry', [FrontController::class, 'entry'])->name('post');
