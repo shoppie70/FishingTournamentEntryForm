5cc57688a2b14044224e3e4233ad5103b829a3c0
Sho Tsukamoto <sho.tsuka.0709@gmail.com>
2022-07-23 14:02:49 +0900

    Fixed Admin Dashboard Path.

--- Modules/Admin/Resources/views/partials/sidebar.blade.php
+++ Modules/Admin/Resources/views/partials/sidebar.blade.php
@@ -48,7 +48,7 @@
             </h6>
             <ul class="md:flex-col md:min-w-full flex flex-col list-none">
                 <li class="items-center">
-                    <a href="/"
+                    <a href="/admin"
                        class="text-xs uppercase py-3 font-bold block　text-pink-500 hover:text-pink-600">
                         <i class="fas fa-tv mr-2 text-sm opacity-75 text-blueGray-300"></i>
                         応募者一覧
