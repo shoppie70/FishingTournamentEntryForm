0edcb20c4b09c656524fde3752faca9fc1a5d67f
Sho Tsukamoto <sho.tsuka.0709@gmail.com>
2022-07-25 10:19:00 +0900

    Added `is_hidden` param in Entry Index.

--- Modules/Admin/Http/Controllers/DashboardController.php
+++ Modules/Admin/Http/Controllers/DashboardController.php
@@ -17,7 +17,11 @@ class DashboardController extends Controller
     {
         $title = '応募者一覧';
 
-        $entries = Entry::query()->where('is_hidden', 0)->get();
+        $entries = Entry::query()
+            ->where('is_hidden', 0)
+            ->with('selected_fish_1')
+            ->with('selected_fish_2')
+            ->get();
 
         return view('admin::index', compact(
             'title',
